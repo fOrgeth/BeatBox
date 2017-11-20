3bfcba154e6c779df52ac5b0c8dfd9e034e4d103
fOrgeth <ivdovin@mail.ru>
2017-11-20 22:53:58 +0300

    first commit

---
+++ app/src/main/java/com/mcs/th/forge/beatbox/BeatBoxFragment.java
@@ -0,0 +1,9 @@
+package com.mcs.th.forge.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
